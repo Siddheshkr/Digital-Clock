4f4c071acef9ee819b3fdd16872b6e4701dbfde8
Siddhesh Kumar <siddheshkr21@gmail.com>
2023-09-11 00:20:47 +0530

    updated files

--- script.js
+++ script.js
@@ -7,19 +7,15 @@ setInterval(() => {
   let currentTime = new Date();
   hrs.innerHTML =
     (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
-    
-    
-    if(currentTime.getHours() > 12){
-      hrs.innerHTML = ` 0${currentTime.getHours()-12}`;
 
-    }
-  
-   min.innerHTML =
-    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes(); 
+  if (currentTime.getHours() > 12) {
+    hrs.innerHTML = ` ${0 + (currentTime.getHours() - 12)}`;
+  }
+
+  min.innerHTML =
+    (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
   sec.innerHTML =
     (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 
-    AmPm.innerHTML= (currentTime.getHours() < 12 ? "AM" : "PM")
-
-
+  AmPm.innerHTML = currentTime.getHours() < 12 ? "AM" : "PM";
 }, 1000);
